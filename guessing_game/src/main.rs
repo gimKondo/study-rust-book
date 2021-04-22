5757cd07f92ffbe738b7446a08344a1616dce514
Kondo Daisuke <gim0kondo@gmail.com>
2021-04-23 02:11:57 +0900

    :+1: use rand for guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,14 +1,19 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");          // 数を当ててごらん
+    println!("Guess the number!"); // 数を当ててごらん
+    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Please input your guess.");   // ほら、予想を入力してね
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess."); // ほら、予想を入力してね
 
     let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read line");     // 行の読み込みに失敗しました
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line"); // 行の読み込みに失敗しました
 
-    println!("予測結果: {}", guess);     // 次のように予想しました: {}
+    println!("予測結果: {}", guess); // 次のように予想しました: {}
 }
