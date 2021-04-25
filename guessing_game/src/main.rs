1b4d1068a5b08fa329b64f8c74f27e0f98b2ec5f
Kondo Daisuke <gim0kondo@gmail.com>
2021-04-25 19:36:28 +0900

    :+1: loop for guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,19 +8,24 @@ fn main() {
 
     println!("The secret number is: {}", secret_number);
 
-    println!("Please input your guess."); // ほら、予想を入力してね
+    loop {
+        println!("Please input your guess."); // ほら、予想を入力してね
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line"); // 行の読み込みに失敗しました
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line"); // 行の読み込みに失敗しました
 
-    let guess: u32 = guess.trim().parse().expect("Please type a number!");
+        let guess: u32 = guess.trim().parse().expect("Please type a number!");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
 }
