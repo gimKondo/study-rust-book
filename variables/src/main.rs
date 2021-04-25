2be26b7d62cb8359714b62743951b46588613390
Kondo Daisuke <gim0kondo@gmail.com>
2021-04-26 02:30:43 +0900

    :tada: general concept of programming

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -3,4 +3,77 @@ fn main() {
     let x = x + 1;
     let x = x * 2;
     println!("The value of x is: {}", x);
+
+    let guess: u32 = "42".parse().expect("Not a number!");
+    // let guess = "42".parse::<u32>().expect("Not a number!");
+    println!("guess: {}", guess);
+
+    let tup: (i32, f64, u8) = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("tuple: ({}, {}, {})", x, y, z);
+    println!("tuple: ({}, {}, {})", tup.0, tup.1, tup.2);
+
+    // let a = [1, 2, 3, 4, 5];
+    // let index: usize = "10".parse().expect("Not a number!");
+    // let element = a[index];
+    // println!("The value of element is: {}", element);
+
+    another_function();
+
+    // Block is a expression
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
+
+    let x = five();
+    println!("The value of x is: {}", x);
+
+    let x = plus_one(5);
+    println!("The value of x is: {}", x);
+
+    let number = 3;
+    if number < 5 {
+        println!("condition was true");
+    } else {
+        println!("condition was false");
+    }
+
+    // if is a expression
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+    println!("The value of number is: {}", number);
+
+    // while loop
+    let mut number = 3;
+    while number != 0 {
+        println!("{}!", number);
+        number = number - 1;
+    }
+    println!("LIFTOFF!!!");
+
+    // for loop
+    let a = [10, 20, 30, 40, 50];
+    for element in a.iter() {
+        println!("the value is: {}", element);
+    }
+
+    // for with Range
+    for number in (1..4).rev() {
+        println!("{}!", number);
+    }
+    println!("LIFTOFF!!!");
+}
+
+fn another_function() {
+    println!("Another function.");
+}
+
+fn five() -> i32 {
+    5
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
