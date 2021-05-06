90096dc98cae75f53a46d45ec4ab81fc8a517fd8
Kondo Daisuke <gim0kondo@gmail.com>
2021-05-07 01:54:56 +0900

    :sparkles: use slice

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -1,16 +1,20 @@
 fn main() {
-    let mut s = String::from("hello world");
+    let s = String::from("hello world");
     let word = first_word(&s);
-    s.clear();
     println!("word = {}", word);
+
+    let hello = &s[0..5];
+    let world = &s[6..11];
+    println!("{}", hello);
+    println!("{}", world);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &String) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
