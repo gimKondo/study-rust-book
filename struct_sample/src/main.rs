925a9da0d4da1c2c6fc81a68589adb043938c709
Kondo Daisuke <gim0kondo@gmail.com>
2021-05-14 03:07:18 +0900

    :sparkles: struct basic

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -1,3 +1,36 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("some_username1"),
+        active: true,
+        sign_in_count: 1,
+    };
+    println!("Hello, {}!", user1.username);
+    let user2 = build_user(
+        String::from("someone@example.com"),
+        String::from("some_username2"),
+    );
+    println!("Hello, {}!", user2.username);
+    let user3 = User {
+        email: String::from("anotherone@example.com"),
+        username: String::from("another_username1"),
+        ..user1
+    };
+    println!("Hello, {}!", user3.username);
 }
