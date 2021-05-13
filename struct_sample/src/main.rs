4b6c09b10c395c2a2a086f46529f815a64c0f504
Kondo Daisuke <gim0kondo@gmail.com>
2021-05-14 03:27:11 +0900

    :sparkles: tuple struct

--- struct_sample/src/main.rs
+++ struct_sample/src/main.rs
@@ -14,6 +14,9 @@ fn build_user(email: String, username: String) -> User {
     }
 }
 
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
 fn main() {
     let user1 = User {
         email: String::from("someone@example.com"),
@@ -33,4 +36,8 @@ fn main() {
         ..user1
     };
     println!("Hello, {}!", user3.username);
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+    println!("black ({}, {}, {})!", black.0, black.1, black.2);
+    println!("origin ({}, {}, {})!", origin.0, origin.1, origin.2);
 }
