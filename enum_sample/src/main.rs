79bd5143fccc666359ce4a927872ee8d3a66b93b
Kondo Daisuke <gim0kondo@gmail.com>
2021-05-30 20:58:16 +0900

    :sparkles: pattern match of enum

--- enum_sample/src/main.rs
+++ enum_sample/src/main.rs
@@ -18,6 +18,22 @@ impl Message {
     }
 }
 
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
 fn main() {
     let home = IpAddr::V4(String::from("127.0.0.1"));
     println!("Home IP: {:?}", home);
@@ -27,4 +43,6 @@ fn main() {
     msg_quit.call();
     let msg_write = Message::Write(String::from("test"));
     msg_write.call();
+    let coin_value = value_in_cents(Coin::Nickel);
+    println!("Coin value: {}", coin_value);
 }
