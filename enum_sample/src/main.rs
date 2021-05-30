f5ec643b858885c0a25c68bbe99fef6d8709ea61
Kondo Daisuke <gim0kondo@gmail.com>
2021-05-30 19:55:43 +0900

    :+1: kind and value pair by enum

--- enum_sample/src/main.rs
+++ enum_sample/src/main.rs
@@ -1,17 +1,12 @@
-enum IpAddrKind {
-    V4,
-    V6,
-}
-
-struct IpAddr {
-    kind: IpAddrKind,
-    address: String,
+#[derive(Debug)]
+enum IpAddr {
+    V4(String),
+    V6(String),
 }
 
 fn main() {
-    let home = IpAddr {
-        kind: IpAddrKind::V4,
-        address: String::from("127.0.0.1"),
-    };
-    println!("Home IP: {}", home.address);
+    let home = IpAddr::V4(String::from("127.0.0.1"));
+    println!("Home IP: {:?}", home);
+    let loop_back = IpAddr::V6(String::from("127.0.0.1"));
+    println!("Home IP: {:?}", loop_back);
 }
