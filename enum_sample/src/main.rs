689fe405b05678f8377beb4848d3f5be9359d7a2
Kondo Daisuke <gim0kondo@gmail.com>
2021-05-30 21:15:36 +0900

    :+1: use multi lines on match

--- enum_sample/src/main.rs
+++ enum_sample/src/main.rs
@@ -27,7 +27,10 @@ enum Coin {
 
 fn value_in_cents(coin: Coin) -> u32 {
     match coin {
-        Coin::Penny => 1,
+        Coin::Penny => {
+            println!("Lucky Penny");
+            1
+        }
         Coin::Nickel => 5,
         Coin::Dime => 10,
         Coin::Quarter => 25,
@@ -45,4 +48,5 @@ fn main() {
     msg_write.call();
     let coin_value = value_in_cents(Coin::Nickel);
     println!("Coin value: {}", coin_value);
+    value_in_cents(Coin::Penny);
 }
