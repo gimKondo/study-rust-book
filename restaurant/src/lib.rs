f7e6c703c78ff35fd1cc4d923cff1f736a3e2a43
Kondo Daisuke <gim0kondo@gmail.com>
2021-06-07 00:02:43 +0900

    :+1: separate mod file

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -6,19 +6,6 @@ mod tests {
     }
 }
 
-mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_wait_list() {}
-        fn seat_at_table() {}
-    }
-
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
-
 fn serve_order() {}
 
 mod back_of_house {
@@ -44,8 +31,9 @@ mod back_of_house {
     fn cook_order() {}
 }
 
+mod front_of_house;
 use self::front_of_house::hosting as self_hosting;
-use crate::front_of_house::hosting;
+pub use crate::front_of_house::hosting;
 use std::collections::HashMap;
 
 pub fn eat_at_restaurant() {
