b9ed0a450096bb65f6ce0083b3e137505e701fa1
Kondo Daisuke <gim0kondo@gmail.com>
2021-06-20 23:23:56 +0900

    :sparkles: entry of HashMap

--- hasmap_sample/src/main.rs
+++ hasmap_sample/src/main.rs
@@ -5,6 +5,7 @@ fn main() {
     scores.insert(String::from("Blue"), 10);
     scores.insert(String::from("Yellow"), 50);
     scores.insert(String::from("Yellow"), 51); // over write
+    scores.entry(String::from("Yellow")).or_insert(54);
     println!("Colors: {:?}", scores);
 
     let team_name = String::from("Blue");
@@ -27,4 +28,12 @@ fn main() {
     println!("map: {:?}", map);
     // println!("map: {}: {}", field_name, field_value);
     // field_name cause error `value borrowed here after move`
+
+    let text = "hello world wonderful world";
+    let mut map = HashMap::new();
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
+    }
+    println!("{:?}", map);
 }
