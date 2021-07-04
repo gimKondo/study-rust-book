5a59b8590d473416f115d04ee7ba1c0c95d921c1
Kondo Daisuke <gim0kondo@gmail.com>
2021-07-05 02:22:39 +0900

    :+1: handle file open error

--- result_example/src/main.rs
+++ result_example/src/main.rs
@@ -2,5 +2,10 @@ use std::fs::File;
 
 fn main() {
     let f = File::open("hello.txt");
-    println!("{:?}", f);
+    match f {
+        Ok(file) => file,
+        Err(error) => {
+            panic!("There was a problem opening th file: {:?}", error)
+        }
+    };
 }
